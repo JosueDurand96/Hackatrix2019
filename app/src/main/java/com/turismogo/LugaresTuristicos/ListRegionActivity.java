4cf83e03157f58f40c7b703d4a606d35d3139169
JosueDurand96 <josue.durand8@gmail.com>
2019-06-01 13:54:09 -0500

    listar region

---
+++ app/src/main/java/com/turismogo/LugaresTuristicos/ListRegionActivity.java
@@ -0,0 +1,15 @@
+package com.turismogo.LugaresTuristicos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.turismogo.R;
+
+public class ListRegionActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_list_region);
+  }
+}
--- app/src/main/java/com/turismogo/Principal/LoginActivity.java
+++ app/src/main/java/com/turismogo/Principal/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.turismogo.Principal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.turismogo.LugaresTuristicos.ListRegionActivity;
 import com.turismogo.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+  TextView txtsaltar;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+    txtsaltar=(TextView)findViewById(R.id.txtsaltar);
+    txtsaltar.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(LoginActivity.this, ListRegionActivity.class);
+        startActivity(intent);
+      }
+    });
   }
 }
